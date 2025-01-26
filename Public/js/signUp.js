0a8fd91231feb6afde606b6fc92c67745bf5a23e
Teddystone08 <ernestlrty@hotmail.com>
2025-01-26 21:14:57 +0000

    Update signUp.js

---
+++ Public/js/signUp.js
@@ -0,0 +1,30 @@
+const signUpForm = document.getElementById("signform");
+ 
+
+// attach the submit handler
+signUpForm.addEventListener('submit', async(event) => {
+    event.preventDefault();
+
+    //gather all the info. from the form
+    const userName = doc.getElementById("username").value;
+    const email = doc.getElementById("email").value;
+    const password = doc.getElementById("password").value;
+
+    const userInfo = {
+        username: userName,
+        email: email,
+        password: password
+    };
+    
+    //make a request to the backend server
+    const result = await fetch (
+        'http://127.0.0.1:3000/auth/register',
+        {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInfo),
+        }
+    ) 
+})
